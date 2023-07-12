b405717ef21deb47f9ae6afe906baba4acd807ed
Glitch (spice-linen-lamb) <none>
2023-07-12 09:05:34 +0000

    🚒🕛 Updated with Glitch

--- script.js
+++ script.js
@@ -33,14 +33,80 @@ filer.forEach((f) => {
   f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
 });
 
-
 // TIKTOK CODE START HERE
+const currency = "USD";
+var user_email = "ENTER YOUR EMAIL HERE";
+var event_id = "Unset";
+var product = null;
 
-var value = 1000;
-var currency = "USD";
-var SKU = "ECABCCCCEDO";
 
+// Demo function for grabing clickid
+const queryString = window.location.search;
+console.log(queryString);
+const urlParams = new URLSearchParams(queryString);
+if(urlParams.has('ttclid')){
+  const ttclid = urlParams.get('ttclid')
+  window.alert("Your ttclid is being captured and it is: "+ttclid);
+}
 
-document.getElementById("value").innerHTML = value;
-document.getElementById("currency").innerHTML = currency;
-document.getElementById("SKU").innerHTML = SKU;
+window.onload = function() {
+  
+  // Initlize page product
+  function load_product(json) {
+    product = json
+    document.getElementById("value").innerHTML = product.price;
+    document.getElementById("SKU").innerHTML = product.id;
+    document.getElementById("title").innerHTML = product.title;
+    document.getElementById("description").innerHTML = product.description;
+    document.getElementById("product_img").src = product.image;
+    
+    // create unique transaction id for it
+    event_id = uuidv4();
+    console.log(event_id);
+    document.getElementById("event_id").innerHTML = event_id;
+    document.getElementById("currency").innerHTML = currency;
+    document.getElementById("user_email").innerHTML = user_email;
+  } 
+  
+  // Fetch some product from some fake api
+  fetch('https://fakestoreapi.com/products/1')
+            .then(res=>res.json())
+            .then(json=>load_product(json))
+  
+  // BONUS: Fetch mutilple product
+  // fetch('https://fakestoreapi.com/products?limit=2')
+  //           .then(res=>res.json())
+  //           .then(json=>console.log(json))
+  
+  
+  // A helper function generate a uuid (Universal Unique Identifier)
+  function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
+
+  // Regen event_id after button click
+  // In real world scanrio, a uuid/sessionid/transactionid could be used for event id
+  document.getElementById("refresh_button").addEventListener('click',function() { 
+    event_id = uuidv4();
+    document.getElementById("event_id").innerHTML = event_id;
+  });
+
+  function set_user_email(){
+    user_email=document.getElementById("user_email").value;
+    console.log(user_email);
+  }
+  
+  document.getElementById("buy").addEventListener('click',function() { 
+        document.getElementById("wait").src = "https://i.gifer.com/1LBN.gif";
+    // ADDING YOUR PURCHASE EVENT CODE HERE
+    
+    
+    
+    // Pretend I am calling my own api server for the buying request, which takes 5 s
+    setTimeout(function(){
+      window.location.href="/ThankYou.html";
+                         }, 5000);
+  });
+}
